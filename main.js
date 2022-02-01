3114d3b73c48db8b85613af1f2620460067fc55f
devailtonweb <devailtonweb@gmail.com>
2022-01-31 22:13:25 -0300

    Ajusta responsividade

--- main.js
+++ main.js
@@ -18,13 +18,13 @@ for (const link of links) {
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', () => {
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
@@ -32,7 +32,13 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination'
   },
   mousewhell: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 const scroll = ScrollReveal({
@@ -47,6 +53,50 @@ ScrollReveal().reveal(
   #about .image, #home .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testmonials,
-  #contact .text, #contact .links`,
+  #contact .text, #contact .links,
+  footer .bland, footer .social`,
   { interval: 100 }
 )
+
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageXOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    console.log('sd')
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+window.addEventListener('scroll', function () {
+  backToTop()
+  changeHeaderWhenScroll()
+  activateMenuAtCurrentSection()
+})
